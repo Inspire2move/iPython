097a197702b461add92ccd19cb9d6bb2bb4b3cea
Inspire2move <inspire2move@gmail.com>
2016-02-02 09:22:43 +0100

    vijfde poging

--- testfile for figures.py
+++ testfile for figures.py
@@ -1,6 +1,17 @@
-figure()
-plot(x, y, 'r')
-xlabel('x')
-ylabel('y')
-title('title')
-show()
+import matplotlib as mp
+
+#%matplotlib inline
+
+#figure()
+#plot(x, y, 'r')
+#xlabel('x')
+#ylabel('y')
+#title('title')
+#show()
+
+
+
+hello = 'Hello'
+world = 'World'
+
+print(hello + ' ' + world)
